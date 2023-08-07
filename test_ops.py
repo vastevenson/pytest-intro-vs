3c3929b31722e1fc90b3455a3b8137d5c24a93d7
malladhiraj <40905963+malladhiraj@users.noreply.github.com>
2023-08-07 03:37:54 +0000

    Update test_ops.py

--- test_ops.py
+++ test_ops.py
@@ -4,10 +4,10 @@ def test_add():
     assert add(2,3) == 6
 
 def test_subtract():
-    assert subtract(2, 3) == -1
+    assert subtract(2, 3) == -2
 
 def test_multiply():
-    assert multiply(2, 3) == 6
+    assert multiply(2, 3) == 5
 
 def test_divide():
-    assert divide(10,5) == 2
+    assert divide(10,5) == 3
